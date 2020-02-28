d73b6de55c35befcf2189a10f0973fde8bdb3979
EmiyaYang <1038810929@qq.com>
2020-02-28 20:56:12 +0800

    chore: add a sample

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,70 @@
 <template>
   <div id="app">
+    <h1>HelloWorld</h1>
+
     <HelloWorld />
-    <ProTable />
+
+    <h1>ProTable</h1>
+    <ProTable :columns="columns" :dataSource="dataSource" />
+
+    <h1>ChartsPane</h1>
+    <ChartsPane />
   </div>
 </template>
 
 <script>
 import ProTable from "../lib/ProTable";
 import HelloWorld from "../lib/HelloWorld";
+import ChartsPane from "../lib/ChartsPane";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
-    ProTable
+    ProTable,
+    ChartsPane
+  },
+  data() {
+    return {
+      columns: [
+        {
+          title: " ",
+          customRender: (value, row, index) => <span>{index + 1}</span>
+        },
+        {
+          title: "频道规模",
+          dataIndex: "a"
+        },
+        {
+          title: "同时在线人数",
+          dataIndex: "b"
+        },
+        {
+          title: "占比",
+          dataIndex: "c"
+        }
+      ],
+      dataSource: [
+        {
+          key: 1,
+          a: 2,
+          b: 36564,
+          c: 4
+        },
+        {
+          key: 2,
+          a: 2,
+          b: 3,
+          c: 4
+        },
+        {
+          key: 3,
+          a: 2,
+          b: 3,
+          c: 4
+        }
+      ]
+    };
   }
 };
 </script>
